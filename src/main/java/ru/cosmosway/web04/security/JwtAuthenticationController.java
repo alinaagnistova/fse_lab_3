cae0228c6a63c4328517c115aa2fb07b9f214245
cosmos <kostyakiselev@gmail.com>
2023-12-18 02:59:46 +0300

    маленький ад)

--- src/main/java/ru/cosmosway/web04/security/JwtAuthenticationController.java
+++ src/main/java/ru/cosmosway/web04/security/JwtAuthenticationController.java
@@ -1,61 +1,67 @@
-//package ru.cosmosway.web04.security;
-//
-//import java.util.Objects;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.BadCredentialsException;
-//import org.springframework.security.authentication.DisabledException;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.web.bind.annotation.*;
-//
-//import ru.cosmosway.web04.services.SessionUserDetails;
-//
-////import com.javainuse.config.JwtTokenUtil;
-////import com.javainuse.model.JwtRequest;
-////import com.javainuse.model.JwtResponse;
-//
-//@RestController
-////@CrossOrigin
-//public class JwtAuthenticationController {
-//
-//    @Autowired
-//    private AuthenticationManager authenticationManager;
-//
-//    @Autowired
-//    private JwtTokenUtil jwtTokenUtil;
-//
-//    @Autowired
-//    private SessionUserDetails sessionUserDetails;
-//
-////    @PostMapping( "/login")
-////    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest)
-////            throws Exception {
-////
-////        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
-////
-////        final UserDetails userDetails = sessionUserDetails
-////                .loadUserByUsername(authenticationRequest.getUsername());
-////
-////        final String token = jwtTokenUtil.generateToken(userDetails);
-////
-////        return ResponseEntity.ok(new JwtResponse(token));
-////    }
-//
-//    private void authenticate(String username, String password) throws Exception {
-//        Objects.requireNonNull(username);
-//        Objects.requireNonNull(password);
-//
-//        try {
-//            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
-//        } catch (DisabledException e) {
-//            throw new Exception("USER_DISABLED", e);
-//        } catch (BadCredentialsException e) {
-//            throw new Exception("INVALID_CREDENTIALS", e);
-//        }
-//    }
-//}
-//
+package ru.cosmosway.web04.security;
+
+import java.util.Objects;
+
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.web.bind.annotation.*;
+
+import ru.cosmosway.web04.services.SessionUserDetails;
+
+//import com.javainuse.config.JwtTokenUtil;
+//import com.javainuse.model.JwtRequest;
+//import com.javainuse.model.JwtResponse;
+
+@Log
+@RestController
+public class JwtAuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private SessionUserDetails sessionUserDetails;
+//    @CrossOrigin
+@RequestMapping(value = "/login", method = RequestMethod.POST)
+    public ResponseEntity<?> createAuthenticationToken(@RequestBody JwtRequest authenticationRequest)
+            throws Exception {
+    log.info(authenticationRequest.toString());
+    log.info("createAuthenticationToke1111n!!!!!§!");
+
+        authenticate(authenticationRequest.getUsername(), authenticationRequest.getPassword());
+    log.info("createAuthenticationToken!!!!!§!");
+
+        final UserDetails userDetails = sessionUserDetails
+                .loadUserByUsername(authenticationRequest.getUsername());
+    log.info("createAuthenticationToken!!!!22222!!");
+
+//    final String token = jwtTokenUtil.generateToken(userDetails);
+//        log.info("createAuthenticationToken!!!!!!");
+
+        return ResponseEntity.ok("ok");
+    }
+
+    private void authenticate(String username, String password) throws Exception {
+        Objects.requireNonNull(username);
+        Objects.requireNonNull(password);
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(username, password));
+        } catch (DisabledException e) {
+            throw new Exception("USER_DISABLED", e);
+        } catch (BadCredentialsException e) {
+            throw new Exception("INVALID_CREDENTIALS", e);
+        }
+    }
+}
+
--- src/main/java/ru/cosmosway/web04/security/SecurityConfig.java
+++ src/main/java/ru/cosmosway/web04/security/SecurityConfig.java
@@ -1,110 +1,116 @@
-//package ru.cosmosway.web04.security;
-//
-//import lombok.RequiredArgsConstructor;
-//import lombok.extern.java.Log;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.authentication.AuthenticationManager;
-//import org.springframework.security.authentication.ProviderManager;
-//import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
-//import org.springframework.security.config.Customizer;
-//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.http.SessionCreationPolicy;
-//import org.springframework.security.core.userdetails.User;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.core.userdetails.UserDetailsService;
-//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-//import org.springframework.security.crypto.password.PasswordEncoder;
-//import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-//import org.springframework.security.web.SecurityFilterChain;
-//import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-//import ru.cosmosway.web04.services.SessionUserDetails;
-//
-//import com.auth0.jwt.JWT;
-//import com.auth0.jwt.algorithms.Algorithm;
-//
-//import static org.springframework.security.config.Customizer.withDefaults;
-//
-//@Log
-//@Configuration
-//@EnableWebSecurity
-//@RequiredArgsConstructor
-//public class SecurityConfig {
-//
-//    //    private final SessionUserDetails userDetails;
-//    private final BCryptPasswordEncoder bCryptPasswordEncoder;
-//    private final JwtAuthenticationFilter jwtAuthenticationFilter;
-//
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http.
-//                authorizeHttpRequests((authorize) -> authorize
-//                                .requestMatchers("/login").permitAll())
-//                csrf((csrf) -> csrf.disable())
+package ru.cosmosway.web04.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.java.Log;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import ru.cosmosway.web04.services.SessionUserDetails;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Log
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+        private final SessionUserDetails userDetails;
+    private final BCryptPasswordEncoder bCryptPasswordEncoder;
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .cors(withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/login").permitAll()
+                        .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
+                )
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers("/*").permitAll()
+                        .anyRequest().authenticated()
+                ).sessionManagement(sessionManagement ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+//        http
 //                .authorizeHttpRequests((authorize) -> authorize
-//                        .requestMatchers("/*").permitAll()
+//                        .requestMatchers("/authenticate").permitAll()
 //                        .anyRequest().authenticated()
-//                );
+//                ).sessionManagement(sessionManagement ->
+//                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+//        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 //
 //        return http.build();
-////        http
-////                .authorizeHttpRequests((authorize) -> authorize
-////                        .requestMatchers("/authenticate").permitAll()
-////                        .anyRequest().authenticated()
-////                ).sessionManagement(sessionManagement ->
-////                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-////        http.addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
-////
-////        return http.build();
-//    }
-////    @Bean
-////    public AuthenticationManager authenticationManager(
-////            SessionUserDetails userDetailsService,
-////            PasswordEncoder passwordEncoder) {
-////        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-////        authenticationProvider.setUserDetailsService(this.userDetails);
-////        authenticationProvider.setPasswordEncoder(this.bCryptPasswordEncoder);
-////
-////        return new ProviderManager(authenticationProvider);
-////    }
-////    @Autowired
-////    public void configure(AuthenticationManagerBuilder builder) throws Exception {
-////        builder.userDetailsService(userDetails).passwordEncoder(bCryptPasswordEncoder);
-////    }
-//
-//    //    @Bean
-////    public PasswordEncoder passwordEncoder() {
-////        return new BCryptPasswordEncoder();
-////    }
+    }
 //    @Bean
 //    public AuthenticationManager authenticationManager(
-//            UserDetailsService userDetailsService,
+//            SessionUserDetails userDetailsService,
 //            PasswordEncoder passwordEncoder) {
 //        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
-//        authenticationProvider.setUserDetailsService(userDetailsService());
-//        authenticationProvider.setPasswordEncoder(passwordEncoder);
+//        authenticationProvider.setUserDetailsService(this.userDetails);
+//        authenticationProvider.setPasswordEncoder(this.bCryptPasswordEncoder);
 //
 //        return new ProviderManager(authenticationProvider);
 //    }
-//
-//    @Bean
-//    public UserDetailsService userDetailsService() {
-//        UserDetails userDetails = User.withDefaultPasswordEncoder()
-//                .username("user")
-//                .password("password")
-//                .roles("USER")
-//                .build();
-//
-//        return new InMemoryUserDetailsManager(userDetails);
+//    @Autowired
+//    public void configure(AuthenticationManagerBuilder builder) throws Exception {
+//        builder.userDetailsService(userDetails).passwordEncoder(bCryptPasswordEncoder);
 //    }
-//
-//    @Bean
+
+    //    @Bean
 //    public PasswordEncoder passwordEncoder() {
-//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//        return new BCryptPasswordEncoder();
 //    }
-//
-//}
+    @Bean
+    public AuthenticationManager authenticationManager(
+            UserDetailsService userDetailsService,
+            PasswordEncoder passwordEncoder) {
+        DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+        authenticationProvider.setUserDetailsService(userDetails);
+        authenticationProvider.setPasswordEncoder(passwordEncoder);
+
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+
+        return new InMemoryUserDetailsManager(userDetails);
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+         return new BCryptPasswordEncoder();
+    }
+
+}
