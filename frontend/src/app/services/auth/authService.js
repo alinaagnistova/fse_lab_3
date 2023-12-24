2a2b0a92354fe7b46005411a6279d32a65226616
Cosmos <kostyakiselev@gmail.com>
2023-12-24 03:59:49 +0300

    docker changes

--- frontend/src/app/services/auth/authService.js
+++ frontend/src/app/services/auth/authService.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
     reducerPath: 'authApi',
     baseQuery: fetchBaseQuery({
-        baseUrl: 'http://172.28.17.153:2346',
+        baseUrl: 'http://localhost:8181',
         prepareHeaders: (headers, {getState}) => {
             const token = getState().auth.userToken
             if (token) {
